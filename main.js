641f0c9110afc6f3c57f41d86243b123147538cd
Vinay2009 <91785104+Vinay2009@users.noreply.github.com>
2022-02-09 16:45:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,23 +9,26 @@ color = "black";
  {
      color = document.getElementById("color").value;
      width_of_line = document.getElementById("width_of_line").value;
-     mouseEvent = "mouseDown";
+     mouseEvent = "mousedown";
  }
  canvas.addEventListener("mousemove", my_mousemove);
- function my_touchmove(e)
+ function my_mousemove(e)
  {
      var current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
      var current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-     if (mouseEvent == "mouseDown") {
-         ctx.beginPath();
-         ctx.strokestyle = color;
-         ctx.lineWidth = width_of_line;
+     if (mouseEvent == "mousedown") {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
 
-         console.log("Last position of x and y coordinates = ");
-         console.log("x =" + current_position_of_mouse_x + "y =" +current_position_of_mouse_y);
-         ctx.moveTo(last_position_of_x, last_position_of_y);
+        console.log("Last position of x and y coordinates = ");
+        console.log("x =" + last_position_of_x + "y =" +last_position_of_y);
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+
+        console.log("Current position of x and y coordinates = ");
+        console.log("x =" + current_position_of_mouse_x + "y =" +current_position_of_mouse_y);
         ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-         ctx.stroke();
+        ctx.stroke();
      }
      last_position_of_x = current_position_of_mouse_x;
      last_position_of_y = current_position_of_mouse_y;
@@ -33,7 +36,7 @@ color = "black";
  canvas.addEventListener("mouseup", my_mouseup);
  function my_mouseup(e)
   { 
-      mouseEvent = "mouseUP"; 
+      mouseEvent = "mouseup"; 
     } 
  canvas.addEventListener("mouseleave", my_mouseleave);
   function my_mouseleave(e)
@@ -43,3 +46,34 @@ color = "black";
      //Additional Activity 
      function clearArea()
       { ctx.clearRect(0, 0, canvas.width, canvas.height);}
+      canvas.addEventListener("touchstart", my_touchstart);
+      function my_touchstart(e)
+      {
+          console.log("my_touchstart");
+          color = document.getElementById("color").value;
+     width_of_line = document.getElementById("width_of_line").value;
+     last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
+     last_position_of_y=e.touches[0].clientY-canvas.offsetTop;
+     }
+     canvas.addEventListener("touchmove", my_touchmove);
+     function my_touchmove(e)
+     {
+         console.log("my_touchmove");
+         current_position_of_touch_x = e.touches[0].clientX-canvas.offsetLeft;
+         current_position_of_touch_y = e.touches[0].clientY-canvas.offsetTop;
+         ctx.beginPath();
+         ctx.strokeStyle = color;
+         ctx.lineWidth = width_of_line;
+
+         console.log("Last position of x and y coordinates = ");
+         console.log("x =" + last_position_of_x + "y =" +last_position_of_y);
+         ctx.moveTo(last_position_of_x, last_position_of_y);
+
+         console.log("Current position of x and y coordinates = ");
+         console.log("x =" + current_position_of_touch_x + "y =" +current_position_of_touch_y);
+         ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+         ctx.stroke();
+        }
+
+
+    
